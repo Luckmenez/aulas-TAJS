356375bf60cd7642bb529db467ea121feb810992
Lucas Menezes <luckmenez@gmail.com>
2024-01-30 00:13:56 -0300

    feat: finished spies module

---
+++ modulo03-conceitos-tajs/aula-03-spies/tests/user.test.js
@@ -0,0 +1,58 @@
+import { describe, it, expect, beforeEach, jest } from "@jest/globals";
+import Service from "../src/service.js";
+import fs from "node:fs/promises";
+import crypto from "node:crypto";
+import { assert } from "node:console";
+
+describe("#Service Test Suite", () => {
+  let _service;
+  const fileName = "testfile.ndjson";
+  const MOCKED_VALUE_PWD = "HASHED_PWD";
+  describe("#Create - Spies", () => {
+    beforeEach(() => {
+      jest.spyOn(crypto, crypto.createHash.name).mockReturnValue({
+        update: jest.fn().mockReturnThis(),
+        digest: jest.fn().mockReturnValue(MOCKED_VALUE_PWD),
+      });
+
+      jest.spyOn(fs, fs.appendFile.name).mockResolvedValue();
+      _service = new Service({ fileName });
+    });
+
+    it("should call appendFiles with right params", async () => {
+      // Arrange
+      const expectedCreatedAt = new Date().toISOString();
+
+      const input = {
+        username: "test",
+        password: "test",
+      };
+
+      jest
+        .spyOn(Date.prototype, Date.prototype.toISOString.name)
+        .mockReturnValue(expectedCreatedAt);
+
+      // Act
+      const result = await _service.create(input);
+
+      // Assert
+      expect(crypto.createHash).toHaveBeenCalledTimes(1);
+      expect(crypto.createHash).toHaveBeenCalledWith("sha256");
+      expect(crypto.createHash("sha256").update).toHaveBeenCalledWith(
+        input.password
+      );
+      expect(
+        crypto.createHash("sha256").update(input.password).digest
+      ).toHaveBeenCalledWith("hex");
+
+      const expected = JSON.stringify({
+        ...input,
+        password: MOCKED_VALUE_PWD,
+        createdAt: expectedCreatedAt,
+      }).concat("\n");
+
+      expect(fs.appendFile).toHaveBeenCalledTimes(1);
+      expect(fs.appendFile).toHaveBeenCalledWith(fileName, expected);
+    });
+  });
+});
